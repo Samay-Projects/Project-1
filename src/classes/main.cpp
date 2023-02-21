3f2a2576df7d5eb3f18625f9c1684d20918615d1
psysm17 <psysm17@nottingham.ac.uk>
2023-02-21 15:55:24 +0000

    feat: beginning code skeleton added

---
+++ src/classes/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "object1/BankAccount.h"
+
+using namespace std;
+int main() {
+    BankAccount *x = new BankAccount();
+    return 0;
+}
---
+++ src/classes/object1/BankAccount.h
@@ -0,0 +1,29 @@
+//
+// Created by Samay Magecha on 21/02/2023.
+//
+
+#ifndef SRC_BANKACCOUNT_H
+#define SRC_BANKACCOUNT_H
+#define MINIMUM_FEE 50
+
+class BankAccount {
+private:
+    double balance;
+    double deposit;
+private:
+    double getDeposit() const;
+    void setDeposit(double d);
+
+public:
+    BankAccount();
+
+public:
+    double getBalance() const;
+    void setBalance(double val);
+public:
+    void withdraw_balance(double val);
+    void deposit_balance(double val);
+};
+
+
+#endif //SRC_BANKACCOUNT_H
