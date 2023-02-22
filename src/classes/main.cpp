1b317572a5aaaf08da809f0f2cd9612aee5db5df
psysm17 <psysm17@nottingham.ac.uk>
2023-02-22 16:15:36 +0000

    fix: fixed infinite loop from faulty validation     cleared cin     ignored faulty characters from cin

--- src/classes/main.cpp
+++ src/classes/main.cpp
@@ -8,7 +8,6 @@ using namespace std;
 int main() {
     auto *currentAccount = new BankAccount();
     bool bankAccountOpen = true;
-    bool invalidInput = false;
     bool success = false;
     int inputVal;
     string instr;
@@ -23,56 +22,57 @@ int main() {
         strmap->setStr(to_string(inputVal));
         if (!strmap->isInt() || inputVal < 1 || inputVal > 4){
             cout << "Invalid Selection (please type the number of the action you wish to take)" << endl;
-            continue;
-        }
-        while (!invalidInput && !success) {
-            try {
-                switch (inputVal) {
-                    case (1):
-                        cout << "Please enter how much you would like to withdraw or type BACK" << endl;
-                        cin >> instr;
-                        if (instr == "BACK"){
-                            success = true;
-                            break;
-                        }
-                        currentAccount->withdraw_funds(stod(instr));
-                        break;
-                    case (2):
-                        cout << "Please enter how much you would like to deposit or type BACK" << endl;
-                        cin >> instr;
-                        if (instr == "BACK"){
-                            success = true;
-                            break;
-                        }
-                        currentAccount->deposit_funds(stod(instr));
-                        break;
-                    case (3):
-                        cout << "Type BACK once you are done" << endl;
-                        currentAccount->check_balance();
-                        cin >> instr;
-                        while (instr != "BACK") {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            while (!success) {
+                try {
+                    switch (inputVal) {
+                        case (1):
+                            cout << "Please enter how much you would like to withdraw or type BACK" << endl;
                             cin >> instr;
                             if (instr == "BACK"){
                                 success = true;
                                 break;
                             }
-                        }
-                        success = true;
-                        break;
-                    case (4):
-                        bankAccountOpen = false;
-                        cout << "Signed Out";
-                        break;
-                    default:
-                        break;
+                            currentAccount->withdraw_funds(stod(instr));
+                            break;
+                        case (2):
+                            cout << "Please enter how much you would like to deposit or type BACK" << endl;
+                            cin >> instr;
+                            if (instr == "BACK"){
+                                success = true;
+                                break;
+                            }
+                            currentAccount->deposit_funds(stod(instr));
+                            break;
+                        case (3):
+                            cout << "Type BACK once you are done" << endl;
+                            currentAccount->check_balance();
+                            cin >> instr;
+                            while (instr != "BACK") {
+                                cin >> instr;
+                                if (instr == "BACK"){
+                                    success = true;
+                                    break;
+                                }
+                            }
+                            success = true;
+                            break;
+                        case (4):
+                            bankAccountOpen = false;
+                            success = true;
+                            cout << "Signed Out";
+                            break;
+                        default:
+                            break;
+                    }
+                } catch (exception &e) {
+                    cout << "ERROR: Invalid Input" << endl;
                 }
-            } catch (exception &e) {
-                cout << "ERROR: Invalid Input" << endl;
-                invalidInput = true;
             }
+            success = false;
         }
-        invalidInput = false;
-        success = false;
     }
     return 0;
 }
